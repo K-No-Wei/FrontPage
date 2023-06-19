c3c6eeb5ac64314c49f7a74df17dec03dd1e82bc
knowei <2994172661@qq.com>
2023-06-19 14:02:40 +0800

    第二天Minio服务搭建

---
+++ heima-leadnews-test/minio-demo/src/main/java/com/heima/minio/test/MinIOTest.java
@@ -0,0 +1,34 @@
+package com.heima.minio.test;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+
+import java.io.FileInputStream;
+
+public class MinIOTest {
+    public static void main(String[] args) {
+        FileInputStream fileInputStream = null;
+
+        try {
+            fileInputStream = new FileInputStream("D:\\like\\552610.png");
+
+            MinioClient minioClient = MinioClient.builder().credentials("minioadmin", "minioadmin")
+                    .endpoint("http://192.168.91.128:9000")
+                    .build();
+
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object("list.png")
+                    .contentType("image/png")
+                    .bucket("leadnews")
+                    .stream(fileInputStream, fileInputStream.available(), -1)
+                    .build();
+
+            minioClient.putObject(putObjectArgs);
+
+            System.out.println("http://192.168.91.128:9000/leadnews/list.png");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/TestMinio.java
@@ -0,0 +1,28 @@
+package com.heima.minio;
+
+import com.heima.file.service.FileStorageService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+@SpringBootTest(classes = MinIOApplication.class)
+@RunWith(SpringRunner.class)
+public class TestMinio {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Test
+    public void test() throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("D:\\like\\552610.png");
+
+        String path = fileStorageService.uploadImgFile("", "like.png", fileInputStream);
+
+        System.out.println(path);
+    }
+}
