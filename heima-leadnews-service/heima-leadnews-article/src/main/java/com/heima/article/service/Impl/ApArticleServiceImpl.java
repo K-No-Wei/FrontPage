bda6baec17640530dd03c016c2194957788ef25d
knowei <2994172661@qq.com>
2023-06-19 11:06:35 +0800

    第二天文章列表实现

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/Impl/ApArticleServiceImpl.java
@@ -0,0 +1,55 @@
+package com.heima.article.service.Impl;
+
+import com.aliyuncs.utils.StringUtils;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.article.mapper.ApArticleMapper;
+import com.heima.article.service.ApArticleService;
+import com.heima.common.constants.ArticleConstants;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.common.dtos.ResponseResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional
+@Slf4j
+public class ApArticleServiceImpl  extends ServiceImpl<ApArticleMapper, ApArticle> implements ApArticleService {
+
+    // 单页最大加载的数字
+    private final static short MAX_PAGE_SIZE = 50;
+
+    @Autowired
+    private ApArticleMapper apArticleMapper;
+
+    @Override
+    public ResponseResult load(Short loadtype, ArticleHomeDto dto) {
+        Integer size = dto.getSize();
+
+        size = Math.min(size, MAX_PAGE_SIZE);
+        dto.setSize(size);
+
+        if (!loadtype.equals(ArticleConstants.LOADTYPE_LOAD_MORE) && !loadtype.equals(ArticleConstants.LOADTYPE_LOAD_NEW)) {
+            loadtype = ArticleConstants.LOADTYPE_LOAD_MORE;
+        }
+
+        //校验文章频道
+        if (StringUtils.isEmpty(dto.getTag())) {
+            dto.setTag(ArticleConstants.DEFAULT_TAG);
+        }
+
+        //时间设置
+        if (dto.getMaxBehotTime() == null) dto.setMaxBehotTime(new Date());
+        if (dto.getMinBehotTime() == null) dto.setMinBehotTime(new Date());
+
+        List<ApArticle> apArticles = apArticleMapper.loadArticleList(dto, loadtype);
+
+        ResponseResult responseResult = ResponseResult.okResult(apArticles);
+        return responseResult;
+    }
+}
